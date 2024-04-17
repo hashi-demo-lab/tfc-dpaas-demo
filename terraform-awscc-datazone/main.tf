2e066d6254b0f152428b220e927a5be1b0f1192b
Simon Lynch <simon.lynch@hashicorp.com>
2024-04-17 17:12:40 +1000

    update

--- terraform-awscc-datazone/main.tf
+++ terraform-awscc-datazone/main.tf
@@ -1,4 +1,4 @@
-resource "awscc_datazone_domain" "name" {
+resource "awscc_datazone_domain" "this" {
   name = var.datazone_domain_name
   description = var.datazone_description
   domain_execution_role = var.datazone_domain_execution_role_arn
@@ -7,9 +7,9 @@ resource "awscc_datazone_domain" "name" {
 }
 
 
-resource "awscc_datazone_environment_blueprint_configuration" "name" {
+resource "awscc_datazone_environment_blueprint_configuration" "this" {
   for_each = var.environment_blueprints
-  domain_identifier = awscc_datazone_domain.name.id
+  domain_identifier = awscc_datazone_domain.this.id
   enabled_regions = each.value.enabled_regions
   environment_blueprint_identifier = each.value.environment_blueprint_identifier
 }

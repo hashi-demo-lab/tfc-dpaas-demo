248d0dce18ca2e4a30e27df9ba9ecba16d8d95c4
Simon Lynch <simon.lynch@hashicorp.com>
2024-04-18 11:19:06 +1000

    update redshift iam

--- terraform-aws-iam-datazone/main.tf
+++ terraform-aws-iam-datazone/main.tf
@@ -30,3 +30,39 @@ resource "aws_iam_role_policy_attachment" "glue" {
   role       = aws_iam_role.glue.name
   policy_arn = data.aws_iam_policy.glue.arn
 }
+
+
+# create role for redshift with inline policy AmazonDataZoneRedshiftAccess-<region>-<domainId> use assume
+resource "aws_iam_role" "redshift" {
+  name = "AmazonDataZoneRedshiftAccess-${var.region}-${var.datazone_domain_id}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+        {
+            "Sid": "RedshiftTrustPolicyStatement",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "datazone.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole",
+            "Condition": {
+                "StringEquals": {
+                    "aws:SourceAccount": "855831148133"
+                },
+                "ArnEquals": {
+                    "aws:SourceArn": "arn:aws:datazone:us-east-1:855831148133:domain/dzd_b7gonunt1mnycn"
+                }
+            }
+        }
+    ]
+  })
+}
+
+data "aws_iam_policy" "redshift" {
+  name = "AmazonDataZoneRedshiftManageAccessRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "redshift" {
+  role       = aws_iam_role.redshift.name
+  policy_arn = data.aws_iam_policy.redshift.arn
+}
--- terraform-aws-iam-datazone/outputs.tf
+++ terraform-aws-iam-datazone/outputs.tf
@@ -4,3 +4,9 @@
   value       = vault_policy.policies
   description = "Sample helm values file that contains all of the configured paths that were created with this module. This should be used a reference and not a raw input to another object"
 } */
+
+
+output "glue_acces_role_arn" {
+  value       = aws_iam_role.glue.arn
+  description = "The ARN of the IAM role for the DataZone Glue role"
+}

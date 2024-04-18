b56091a3c201f93b6c779f30a73398903e3b20b2
Simon Lynch <simon.lynch@hashicorp.com>
2024-04-18 20:23:02 +1000

    add datazone s3 bucket

--- terraform-awscc-datazone-domain/main.tf
+++ terraform-awscc-datazone-domain/main.tf
@@ -22,6 +22,42 @@ module "datazone_iam" {
 # S3 bucket for DataZone Lake Formation
 # TO DO
 #
+resource "aws_s3_bucket" "datazone" {
+  bucket = "datazone-${awscc_datazone_domain.this.id}-${var.region}"
+}
+
+resource "aws_s3_bucket_public_access_block" "datazone" {
+  bucket = aws_s3_bucket.datazone.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "datazone" {
+  bucket = aws_s3_bucket.datazone.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "datazone" {
+  bucket = aws_s3_bucket.datazone.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "datazone" {
+  bucket = aws_s3_bucket.datazone.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
 
 # Enable Data Zone blueprints

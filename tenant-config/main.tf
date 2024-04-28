1c1c8842f8e968f6fbcd366dbf25784800606a7b
Simon Lynch <simon.lynch@hashicorp.com>
2024-04-29 07:20:39 +1000

    add tenant config

--- tenant-config/main.tf
+++ tenant-config/main.tf
@@ -1,8 +1,21 @@
+# Data source used to grab the TLS certificate for Terraform Cloud.
+data "tls_certificate" "tfc_certificate" {
+  url = "https://${var.tfc_hostname}"
+}
+
+# Creates an OIDC provider which is restricted to
+resource "aws_iam_openid_connect_provider" "tfc_provider" {
+  url             = data.tls_certificate.tfc_certificate.url
+  client_id_list  = [var.tfc_aws_audience]
+  thumbprint_list = [data.tls_certificate.tfc_certificate.certificates[0].sha1_fingerprint]
+}
+
+
 module "project_oidc" {
   source = "github.com/hashi-demo-lab/tfc-dpaas-demo//terraform-aws-oidc-dynamic-creds"
 
-    oidc_provider_arn            = var.oidc_provider_arn
-    oidc_provider_client_id_list = var.oidc_provider_client_id_list
+    oidc_provider_arn            = aws_iam_openid_connect_provider.tfc_provider.arn
+    oidc_provider_client_id_list = [var.tfc_aws_audience]
     tfc_organization_name        = var.tfc_organization_name
     cred_type                    = var.cred_type
     tfc_project_name   = var.tfc_project_name
--- tenant-config/variables.tf
+++ tenant-config/variables.tf
@@ -4,15 +4,22 @@ variable "cred_type" {
     default = "project"
 }
 
-variable "project_name" {
-    type = string
-    description = "(optional) describe your variable"
-}
-
 variable "tfc_organization_name" {
     type = string
     description = "hcp terraform cloud organization name"
     default = "tfc-demo-au"
+}
+
+variable "tfc_hostname" {
+    type = string
+    description = "hcp terraform cloud hostname"
+    default = "app.terraform.io"
+}
+
+variable "tfc_aws_audience" {
+    type = string
+    description = "hcp terraform cloud aws audience"
+    default = "aws.workload.identity"
 }
 
 variable "tfc_project_name" {
@@ -29,10 +36,17 @@ variable "tfc_project_id" {
 variable "oidc_provider_arn" {
     type = string
     description = "oidc provider arn"
+    default = null
 }
 
 variable "oidc_provider_client_id_list" {
     type = list(string)
     description = "oidc provider client id list"
     default = ["aws.workload.identity"]
+}
+
+variable "region" {
+    type = string
+    description = "aws region"
+    default = "ap-southeast-2"
 }

d1ca7b3c368cab5555d89a587d297b11f8397ccd
Simon Lynch <simon.lynch@hashicorp.com>
2024-04-30 15:17:33 +1000

    fix loop

--- tenant-config/main.tf
+++ tenant-config/main.tf
@@ -2,20 +2,21 @@
 locals {
   config_file = flatten([for tenant in fileset(path.module, "config/*.yaml") : yamldecode(file(tenant))])
   tenant      = { for bu in local.config_file : bu.bu => bu }
-  
-  bu_project_keys = merge([
-    for bu_key, bu_value in local.tenant : {
-      for project_key, project_value in bu_value.projects : 
-        "${bu_key}-${project_key}" => project_value
-    }
-  ]...)
 
-}
+  bu_project_list = flatten([
+    for bu_key, bu_value in local.tenant : [
+      for project_key, project_value in bu_value.projects : { "${bu_key}-${project_key}" : {
+        bu      = bu_key
+        project = project_key
+        value   = project_value
+        }
+      }
+    ]
+  ])
 
+  # convert list of bu_project_list to map
+  bu_projects = { for bu_project in local.bu_project_list : keys(bu_project)[0] => values(bu_project)[0] }
 
-
-output "bu_project_keys" {
-  value = local.bu_project_keys
 }
 
 
@@ -33,10 +34,11 @@ resource "aws_iam_openid_connect_provider" "tfc_provider" {
 }
 ############################################
 
-# Create the project in Terraform Cloud
+# Create the project and teams in Terraform Cloud
 module "consumer_project_team" {
   source = "github.com/hashi-demo-lab/terraform-tfe-project-team"
- 
+
+
   organization_name = var.tfc_organization_name
   project_name      = "test1"
   business_unit     = "bu1"
@@ -46,23 +48,22 @@ module "consumer_project_team" {
       team = {
         access      = "read"
         sso_team_id = null
-        }
+      }
     }
   }
-  
-  bu_control_admins_id = ""
-  
-}
 
+  bu_control_admins_id = ""
+
+}
 
 module "project_oidc" {
   source = "github.com/hashi-demo-lab/tfc-dpaas-demo//terraform-aws-oidc-dynamic-creds"
 
-    oidc_provider_arn            = aws_iam_openid_connect_provider.tfc_provider.arn
-    oidc_provider_client_id_list = [var.tfc_aws_audience]
-    tfc_organization_name        = var.tfc_organization_name
-    cred_type                    = var.cred_type
-    tfc_project_name   = var.tfc_project_name
-    tfc_project_id     = var.tfc_project_id
+  oidc_provider_arn            = aws_iam_openid_connect_provider.tfc_provider.arn
+  oidc_provider_client_id_list = [var.tfc_aws_audience]
+  tfc_organization_name        = var.tfc_organization_name
+  cred_type                    = var.cred_type
+  tfc_project_name             = var.tfc_project_name
+  tfc_project_id               = var.tfc_project_id
 
 }
--- tenant-config/outputs.tf
+++ tenant-config/outputs.tf
@@ -1,6 +1,10 @@
 
 output "bu" {
-  value = local.tenant
+  value       = local.tenant
   description = "The name of the business unit projects."
-  
+}
+
+
+output "bu_project_list" {
+  value = local.bu_project_list
 }

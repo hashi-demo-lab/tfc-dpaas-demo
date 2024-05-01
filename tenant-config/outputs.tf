ff0b3887a9b140cf3dac4e28ffa781b71c7f46cb
Simon Lynch <simon.lynch@hashicorp.com>
2024-05-01 15:41:12 +1000

    update outputs

--- tenant-config/outputs.tf
+++ tenant-config/outputs.tf
@@ -1,15 +1,3 @@
-
-output "bu" {
-  value       = local.tenant
-  description = "The name of the business unit projects."
-}
-
-
-output "bu_project_projects" {
-  value = local.bu_projects_access
-}
-
-
 output "projects" {
   value = module.consumer_project
 }

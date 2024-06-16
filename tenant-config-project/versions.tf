3436ba123c462aa9722360313b8f44e130533a98
Simon Lynch <simon.lynch@hashicorp.com>
2024-06-17 09:46:04 +1000

    update versions

--- tenant-config-project/versions.tf
+++ tenant-config-project/versions.tf
@@ -2,7 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.47.0"
+      version = "~> 5.47"
     }
+
+    tfe = {
+      source = "hashicorp/tfe"
+      version = "~> 0.56"
+    }
+
   }
 }
